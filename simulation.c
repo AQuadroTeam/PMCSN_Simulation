250b2f04dc64f43bcd7f40384e654dec27586a62
Ablablab <andrea.gennusa@gmail.com>
2017-02-14 13:07:52 +0100

    added some initialization

--- simulation.c
+++ simulation.c
@@ -28,8 +28,48 @@ double t_current;
 double t_begin;
 double t_end;
 
+void initialize_state()
+{
+  state.cloudlet_1 = 0;
+  state.cloudlet_2 = 0;
+  state.cloud_1 = 0;
+  state.cloud_2 = 0;
+  state.setup_2 = 0;
+}
+
+double generate_exp(mean)
+{
+  return 0.0; //TODO: add custom exp
+}
+
+void initialize_events()
+{
+  push_event(EVENT_ARRIVE1, generate_exp(lambda_1));
+  push_event(EVENT_ARRIVE2, generate_exp(lambda_2));
+}
+
+void process_arrive_1_all(struct Event * event)
+{
+  if(state.cloudlet_1 == N )
+}
+
+void process_event(struct Event * first)
+{
+  switch (first->type):
+  case ARRIVE_1:
+    process_arrive_1(first)
+
+}
+
 int main(int argc, char ** argv)
 {
+  initialize_state();
+  initialize_events();
+  while()
+  {
+    process_event(events);
+
+  }
   return EXIT_SUCCESS;
 
 
