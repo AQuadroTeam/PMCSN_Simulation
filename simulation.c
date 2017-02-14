980c4daf43a8c2c33d90f59795a2985b3a8b1175
Ablablab <andrea.gennusa@gmail.com>
2017-02-14 12:38:16 +0100

    added events type

--- simulation.c
+++ simulation.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+EVENT_ARRIVE1 = 1
+EVENT_ARRIVE2 = 2
+EVENT_COMPLETED_1_IN_1 = 3
+EVENT_COMPLETED_1_IN_2 = 4
+EVENT_COMPLETED_2_IN_1 = 5
+EVENT_COMPLETED_2_IN_2 = 6
+EVENT_COMPLETED_2_IN_SETUP = 7
 
 int main(int argc, char ** argv)
 {
