e1836533a4ce745c9d5d9781abfa8d0026976f91
Ablablab <andrea.gennusa@gmail.com>
2017-02-14 12:50:22 +0100

    added struct for events

--- simulation.c
+++ simulation.c
@@ -8,10 +8,24 @@ EVENT_COMPLETED_2_IN_1 = 5
 EVENT_COMPLETED_2_IN_2 = 6
 EVENT_COMPLETED_2_IN_SETUP = 7
 
-struct event {
+struct Event {
   double time;
   int type;
 }
+
+struct State {
+  int cloudlet_1; //n1
+  int cloudlet_2; //n2
+  int cloud_1;
+  int cloud_2;
+  int setup_2;
+}
+
+struct State state;
+struct Event * events = NULL;
+double t_current;
+double t_begin;
+double t_end;
 
 int main(int argc, char ** argv)
 {
