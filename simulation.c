1ee2c81f45710910e55d7ca70966ea58789c89ca
Ablablab <andrea.gennusa@gmail.com>
2017-02-17 17:31:53 +0100

    added counter for general jobs (useful for debug). Served = Sum completed jobs over each class. Entered = Sum Completed jobs over each class plus sum of jobs in each server at the end of simulation

--- simulation.c
+++ simulation.c
@@ -23,7 +23,7 @@
 #define PATH_2_1 5
 #define PATH_2_2 6
 #define PATH_2_S_2 7
-#define PATH_NOT_TO_UPDATE -1
+#define PATH_NOT_TO_UPDATE -2
 
 #define lambda_1 3.25
 #define lambda_2 6.25
@@ -50,6 +50,8 @@ struct State {
   int cloud_1;
   int cloud_2;
   int setup_2;
+
+
 };
 
 struct State *state;
@@ -57,6 +59,9 @@ double t_current = 0.0;
 double t_begin = 0.0;
 
 long counter_per_path[5];
+// 2 events arrive are always on system
+long counter_generated = -2;
+long counter_exited = 0;
 
 // Parameters
 double t_end = 0.0;
@@ -76,7 +81,8 @@ void set_t(double time){
 
 void print_actual_state()
 {
-  fprintf(stdout, "state at %f: cloudlet (%d,%d), cloud (%d,%d), setup %d. Path completed: cloudlet(%ld,%ld),cloud(%ld,%ld),withSetup(%ld)\n",get_t(),state->cloudlet_1,state->cloudlet_2,state->cloud_1,state->cloud_2,state->setup_2, counter_per_path[0],counter_per_path[2],counter_per_path[1],counter_per_path[3],counter_per_path[4]);
+  fprintf(stdout, "state at %f: cloudlet (%d,%d), cloud (%d,%d), setup %d. Path completed: cloudlet(%ld,%ld),cloud(%ld,%ld),withSetup(%ld). Entered %ld, Served %ld\n",
+    get_t(),state->cloudlet_1,state->cloudlet_2,state->cloud_1,state->cloud_2,state->setup_2, counter_per_path[0],counter_per_path[2],counter_per_path[1],counter_per_path[3],counter_per_path[4],counter_generated,counter_exited);
 }
 
 
@@ -100,7 +106,7 @@ struct Event * generate_completion_event(struct Event * event, double mu, int ne
   if (path != PATH_NOT_TO_UPDATE){
     event->path = path;
   }
-  if(DEBUG){printf("Event Generation: Completion of packet type %d arrived at %f, next time %f\n", next_event_type, event->arrival_time, event->time);}
+  if(DEBUG){printf("Event Generation: Completion of packet type %d path %d arrived at %f, next time %f\n", next_event_type,event->path, event->arrival_time, event->time);}
 
   return event;
 }
@@ -111,6 +117,8 @@ struct Event * generate_arrive_event(double lambda, int EVENT){
     fprintf(stderr, "Error in calloc!\n");
     return NULL;
   }
+  // a job is entered in system, update counter;
+  counter_generated++;
 
   event->next = NULL;
   event->prev = NULL;
@@ -125,6 +133,7 @@ struct Event * generate_arrive_event(double lambda, int EVENT){
 }
 
 void exit_event(struct Event * event){
+  counter_exited++;
   counter_per_path[event->path-3]++;
   if(DEBUG){printf("Event Destroyed: Exited packet with path %d at %f\n", event->path, get_t());}
   free(event);
@@ -136,7 +145,6 @@ int arrive_1_free(struct Event *ev){
   if(DEBUG){printf("\nArrived 1 in free\n");}
   if(DEBUG)print_actual_state();
   state->cloudlet_1++;
-  if(DEBUG)print_actual_state();
 
   //Complete event 1_1
   generate_completion_event(ev, mu_cloudlet_1, EVENT_COMPLETED_1_IN_1, PATH_1_1);
@@ -145,6 +153,8 @@ int arrive_1_free(struct Event *ev){
   //Generate next arrival 1
   struct Event * arrival = generate_arrive_event(lambda_1, EVENT_ARRIVE1);
   push_event(arrival);
+
+  if(DEBUG)print_actual_state();
   return 0;
 }
 
@@ -152,7 +162,7 @@ int arrive_2_free(struct Event *ev){
   if(DEBUG){printf("\nArrived 2 free\n");}
   if(DEBUG)print_actual_state();
   state->cloudlet_2++;
-  if(DEBUG)print_actual_state();
+
 
   //Complete event 2_1
   generate_completion_event(ev, mu_cloudlet_2, EVENT_COMPLETED_2_IN_1, PATH_2_1);
@@ -161,6 +171,9 @@ int arrive_2_free(struct Event *ev){
   //Generate next arrival 2
   struct Event * arrival = generate_arrive_event(lambda_2, EVENT_ARRIVE2);
   push_event(arrival);
+
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -169,10 +182,12 @@ void manage_setup_arrival(struct Event *ev){
   if(DEBUG)print_actual_state();
   state->setup_2--;
   state->cloud_2++;
-  if(DEBUG)print_actual_state();
+
   //Complete event 2_2
   generate_completion_event(ev, mu_cloud_2, EVENT_COMPLETED_2_IN_2, PATH_2_S_2);
   push_event(ev);
+
+  if(DEBUG)print_actual_state();
 }
 
 int setup_free(struct Event *ev){
@@ -184,7 +199,7 @@ int arrive_1_busy(struct Event *ev){
   if(DEBUG){printf("\nArrived 1 in Busy\n");}
   if(DEBUG)print_actual_state();
   state->cloud_1++;
-  if(DEBUG)print_actual_state();
+
   //Complete event 1_2
   generate_completion_event(ev, mu_cloud_1, EVENT_COMPLETED_1_IN_2, PATH_1_2);
   push_event(ev);
@@ -192,6 +207,8 @@ int arrive_1_busy(struct Event *ev){
   //Generate next arrival 1
   struct Event * arrival = generate_arrive_event(lambda_1, EVENT_ARRIVE1);
   push_event(arrival);
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -199,7 +216,7 @@ int arrive_2_busy(struct Event *ev){
   if(DEBUG){printf("\nArrived 2 in Busy\n");}
   if(DEBUG)print_actual_state();
   state->cloud_2++;
-  if(DEBUG)print_actual_state();
+
   //Complete event 2_2
   generate_completion_event(ev, mu_cloud_2, EVENT_COMPLETED_2_IN_2, PATH_2_2);
   push_event(ev);
@@ -207,6 +224,8 @@ int arrive_2_busy(struct Event *ev){
   //Generate next arrival 2
   struct Event * arrival = generate_arrive_event(lambda_2, EVENT_ARRIVE2);
   push_event(arrival);
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -221,7 +240,7 @@ int arrive_1_busy_2(struct Event *ev){
   state->cloudlet_1++;
   state->cloudlet_2--;
   state->setup_2++;
-  if(DEBUG)print_actual_state();
+
   //gen compl_1_1 and compl_setup and arrivo_1
 
   //Complete event 1_1
@@ -236,6 +255,8 @@ int arrive_1_busy_2(struct Event *ev){
   //Generate next arrival 1
   struct Event * arrival = generate_arrive_event(lambda_1, EVENT_ARRIVE1);
   push_event(arrival);
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -243,7 +264,7 @@ int arrive_2_busy_2(struct Event *ev){
   if(DEBUG){printf("\nArrived 2 in Busy 2\n");}
   if(DEBUG)print_actual_state();
   state->cloud_2++;
-  if(DEBUG)print_actual_state();
+
   //Complete event 2_2
   generate_completion_event(ev, mu_cloud_2, EVENT_COMPLETED_2_IN_2, PATH_2_2);
   push_event(ev);
@@ -251,6 +272,8 @@ int arrive_2_busy_2(struct Event *ev){
   //Generate next arrival 2
   struct Event * arrival = generate_arrive_event(lambda_2, EVENT_ARRIVE2);
   push_event(arrival);
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -263,8 +286,11 @@ int compl_1_1(struct Event *ev){
   if(DEBUG){fprintf(stderr,"\nCompletion 1 in Cloudlet\n");}
   if(DEBUG)print_actual_state();
   state->cloudlet_1--;
-  if(DEBUG)print_actual_state();
+
   exit_event(ev);
+
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -272,8 +298,11 @@ int compl_1_2(struct Event *ev){
   if(DEBUG){fprintf(stdout,"\nCompletion 1 in Cloud\n");}
   if(DEBUG)print_actual_state();
   state->cloud_1--;
-  if(DEBUG)print_actual_state();
+
   exit_event(ev);
+
+  if(DEBUG)print_actual_state();
+
   return 0;
 }
 
@@ -281,8 +310,8 @@ int compl_2_1(struct Event *ev){
   if(DEBUG){fprintf(stdout,"\nCompletion 2 in Cloudlet\n");}
   if(DEBUG)print_actual_state();
   state->cloudlet_2--;
-  if(DEBUG)print_actual_state();
   exit_event(ev);
+  if(DEBUG)print_actual_state();
 
   return 0;
 }
@@ -291,8 +320,8 @@ int compl_2_2(struct Event *ev){
   if(DEBUG){fprintf(stdout, "\nCompletion 2 in Cloud\n");}
   if(DEBUG)print_actual_state();
   state->cloud_2--;
-  if(DEBUG)print_actual_state();
   exit_event(ev);
+  if(DEBUG)print_actual_state();
   return 0;
 }
 
