d5e0a3c23b11ab0e5ac7ae765de8d0260a227997
Ablablab <andrea.gennusa@gmail.com>
2017-02-24 19:03:32 +0100

    added comparison governor script

--- comparison_governor_script.sh
+++ comparison_governor_script.sh
@@ -5,14 +5,14 @@ echo "t_1_1_last - t_1_2_last - t_2_1_last - t_2_2_last - t_2_S_2_last - t_waste
 for N in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50;
 do
   echo "$N of 100"
-  ./simulation $N $N 1000 10136 10 0 0 | grep "Ended simulation" -A 4 | grep "Total Mean for path" -A 2 | grep "Total Mean for path" -v >>  governor_comparison_first.data
+    ./simulation $N $N 1000 10136 64 0 0 | grep "Ended simulation" -A 4 | grep "Total Mean for path" -A 2 | grep "Total Mean for path" -v >>  governor_comparison_first.data
 
 done
 for N in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50;
 do
   echo "$N of 100"
 
-  ./simulation $N $N 1000 10136 10 1 0 | grep "Ended simulation" -A 4 | grep "Total Mean for path" -A 2 | grep "Total Mean for path" -v >>  governor_comparison_last.data
+  ./simulation $N $N 1000 10136 64 1 0 | grep "Ended simulation" -A 4 | grep "Total Mean for path" -A 2 | grep "Total Mean for path" -v >>  governor_comparison_last.data
 
 done
 
