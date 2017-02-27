95583947c421971624f664e5ab395c0768b29f95
Ablablab <andrea.gennusa@gmail.com>
2017-02-27 16:55:40 +0100

    updated workspace of distribution of response time

--- script_matlab.m
+++ script_matlab.m
@@ -1,10 +1,10 @@
 exp = fitdist(response_time, 'exponential')
 [a,b,c]=chi2gof(response_time, 'CDF',exp)
 %% count number of jobs
-batch_number = 10;
+batch_number = 64;
 counter_jobs_per_batch = zeros(batch_number,1)
 counter_jobs_per_batch_path = zeros(batch_number, 5)
-data_cell_array = simN20S10batchtimetotal1000
+data_cell_array = simN20S20batchtimetotal1000
 for index = 1:numel(data_cell_array)-1
     
     element = data_cell_array(index,:,:);
@@ -32,13 +32,13 @@ for index = 1:numel(data_cell_array)
 end
 
 %% filter for batch, path, build new arrays
-batch_to_consider = 1
+batch_to_consider = 2
 response_1_1 = zeros(counter_jobs_per_batch_path(batch_to_consider,1),1);
-response_1_2 = zeros(counter_jobs_per_batch_path(batch_to_consider,1),1);
-response_2_1 = zeros(counter_jobs_per_batch_path(batch_to_consider,1),1);
-response_2_2 = zeros(counter_jobs_per_batch_path(batch_to_consider,1),1);
-response_2_S_2 = zeros(counter_jobs_per_batch_path(batch_to_consider,1),1);
-
+response_1_2 = zeros(counter_jobs_per_batch_path(batch_to_consider,2),1);
+response_2_1 = zeros(counter_jobs_per_batch_path(batch_to_consider,3),1);
+response_2_2 = zeros(counter_jobs_per_batch_path(batch_to_consider,4),1);
+response_2_S_2 = zeros(counter_jobs_per_batch_path(batch_to_consider,5),1);
+response_total = zeros(counter_jobs_per_batch(batch_to_consider),1);
 
 
 index_to_add_1_1 = 1;
@@ -46,7 +46,7 @@ index_to_add_1_2 = 1;
 index_to_add_2_1 = 1;
 index_to_add_2_2 = 1;
 index_to_add_2_S_2 = 1;
-
+index_to_add_total = 1;
 for index = 1:numel(data_cell_array)
     
     element = data_cell_array(index,:,:);
@@ -80,6 +80,13 @@ for index = 1:numel(data_cell_array)
         response_2_S_2(index_to_add_2_S_2) = cell2mat(element(3));
         index_to_add_2_S_2 = index_to_add_2_S_2 + 1;
     end
+    
+    % total
+    if cell2mat(element(1)) == batch_to_consider-1
+        response_total(index_to_add_total) = cell2mat(element(3));
+        index_to_add_total = index_to_add_total + 1;
+    end
+    
 end
 
 %% check if for is correct
@@ -115,19 +122,22 @@ if counter_jobs_per_batch_path(batch_to_consider, 2) > 0
     exp_1_2 = fitdist(response_1_2,'exponential') % if 0 creates problems
 end
 if counter_jobs_per_batch_path(batch_to_consider, 3) > 0
-    exp_2_1 = fitdist(response_2_1,'exponential')
+    exp_2_1 = fitdist(response_2_1,'Gamma')
 end
 if counter_jobs_per_batch_path(batch_to_consider, 4) > 0
     exp_2_2 = fitdist(response_2_2,'exponential')
 end
 if counter_jobs_per_batch_path(batch_to_consider, 5) > 0
-    exp_2_S_2 = fitdist(response_2_S_2,'exponential')
+    exp_2_S_2 = fitdist(response_2_S_2,'Gamma')
 end
 
+if counter_jobs_per_batch(batch_to_consider) > 0
+    exp_total = fitdist(response_total,'Exponential')
+end
 
 %%
 if counter_jobs_per_batch_path(batch_to_consider, 1) > 0
-    [a_1_1,b_1_1,c_1_1]=chi2gof(response_1_1, 'CDF',exp_1_1)
+    [a_1_1,b_1_1,c_1_1]=chi2gof(response_1_1, 'CDF',exp_1_1,'Alpha',0.5)
 end
 if counter_jobs_per_batch_path(batch_to_consider, 2) > 0
     [a_1_2,b_1_2,c_1_2]=chi2gof(response_1_2, 'CDF',exp_1_2)
@@ -140,6 +150,9 @@ if counter_jobs_per_batch_path(batch_to_consider, 4) > 0
 end
 if counter_jobs_per_batch_path(batch_to_consider, 5) > 0
     [a_2_S_2,b_2_S_2,c_2_S_2]=chi2gof(response_2_S_2, 'CDF',exp_2_S_2)
+end
+if counter_jobs_per_batch(batch_to_consider) > 0
+    [a_total,b_total,c_total]=chi2gof(response_total, 'CDF',exp_total, 'Alpha', 0.95)
 end
 % mmm, this response_1_1 must be very similar to an exponential, so there's
 % a problem with my use of chi2gof
